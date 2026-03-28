c4134f6f3ef8500ed06f685b96ce6a58115a0bbc
Marcin <m.wladzimiruk12@gmail.com>
2026-03-28 22:15:00 +0100

    second test commit

---
+++ src/main/java/tuKlasy/apiResponse.java
@@ -0,0 +1,13 @@
+package tuKlasy;
+
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+public class apiResponse {
+    Integer id;
+    Integer UserId;
+    String title;
+    String body;
+}
